2eb75a5c6e7b7ded167173f1bc63a6b089d5a092
DanielPythonProjects <34587449+DanielPythonProjects@users.noreply.github.com>
2017-12-15 15:29:42 -0800

    PassGen Python Code
    My password generator

---
+++ PassGen.py
@@ -0,0 +1,11 @@
+import random
+
+print(" ###Welcome to the password generator###")
+chars ='abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ.!@#$%&*'
+num_pass = int(input('How many passwords do you want: '))
+length = int(input('How many characters do you need?: '))
+for p in range(num_pass):
+    password = ''
+    for c in range(length):
+        password += random.choice(chars)
+    print("Password below \n" + password)
