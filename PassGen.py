9d9ae429c64a1f558ea6da9ada62fef566778e6b
DanielPythonProjects <34587449+DanielPythonProjects@users.noreply.github.com>
2018-01-10 14:15:34 -0800

    updated code
    provided latest updates, bug fixes, and also added a Function and try and except in case of error.

--- PassGen.py
+++ PassGen.py
@@ -1,16 +1,29 @@
-' Author: Daniel Godinez
-' Date: 12/15/2017
-' Python Version: 3.6.3
+# Author: Daniel Godinez
+# Date: 12/15/2017
+# PassGen Version 1.1
+# Python Version: 3.6.3
+
 import random
 
+#Introduction to application
 print(" ###Welcome to the Free password generator###")
 print(" ###Provided by Daniel Godinez, danieldba1867@gmail.com###")
+print(" ###With passGen, you can generate as many passwords as you want!###")
 
-chars ='abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ.!@#$%&*'
-num_pass = int(input('How many passwords do you want: '))
-length = int(input('How many characters do you need?: '))
-for p in range(num_pass):
-    password = ''
-    for c in range(length):
-        password += random.choice(chars)
-    print("Password below \n" + password)
+
+def passGen():
+    try:
+        chars ='abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ.!@#$%&*'
+        num_pass = int(input('How many passwords do you want: '))
+        length = int(input('How many characters do you need?: '))
+
+        for p in range(num_pass):
+            password = ''
+            for c in range(length):
+                password += random.choice(chars)
+            print("Password below: \n" + password)
+        print("Come back & have a good day!")
+    except:
+        print("Error, please try again")
+if __name__ == "__main__":
+      passGen()
