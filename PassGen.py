4f65c045b797bb7f6e1fef4049334bc88477010b
DanielPythonProjects <34587449+DanielPythonProjects@users.noreply.github.com>
2018-01-10 13:47:53 -0800

    Updated intro

--- PassGen.py
+++ PassGen.py
@@ -1,9 +1,11 @@
 ' Author: Daniel Godinez
 ' Date: 12/15/2017
-' Python 3.6.3
+' Python Version: 3.6.3
 import random
 
-print(" ###Welcome to the password generator###")
+print(" ###Welcome to the Free password generator###")
+print(" ###Provided by Daniel Godinez, danieldba1867@gmail.com###")
+
 chars ='abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ.!@#$%&*'
 num_pass = int(input('How many passwords do you want: '))
 length = int(input('How many characters do you need?: '))
